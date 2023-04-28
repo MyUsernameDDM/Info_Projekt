ff1ff43e68cdbcee53b55930fe2387eda7d0bcdb
Philipp Roeggl <roeggl.philipp@gmail.com>
2023-04-28 11:04:36 +0200

    Matching, ArrayList wurde zu Array umgeändert, da länge von matchUnits immer maximal 10 ist. Matching auch ausgaben gelöscht

--- src/main/java/MainModel/Matching.java
+++ src/main/java/MainModel/Matching.java
@@ -5,39 +5,36 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.ArrayList;
 
 public class Matching extends Thread {
-    private String str;
+    private final String str;
 
     public Matching(String s) {
         str = s;
     }
 
-    public ArrayList<matchUnits> matches = new ArrayList<>();
+    public matchUnits[] matches = new matchUnits[10];
 
     @Override
     public void run() {
         try {
             URL url = new URL("https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=" + str + "&apikey=" + "QESJBL81ZZ99LAQX");
-            HttpURLConnection con = null;
+            HttpURLConnection con;
             con = (HttpURLConnection) url.openConnection();
             con.setRequestMethod("GET");
             BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-            String input = null;
+            String input;
             int count = 0;
             while (count < 2) {
                 input = in.readLine();
-                System.out.println(input);
                 if (input == null)
                     return;
                 if (input.contains("{")) {
                     count++;
-                    System.out.println(count);
                 }
 
             }
-            while (true) {
+            for (int j = 0; j < matches.length; ++j) {
                 String[] data = new String[9];
                 for (int i = 0; i < data.length; ++i) {
                     input = in.readLine();
@@ -46,19 +43,18 @@ public class Matching extends Thread {
                     }
                     if (input.contains("}"))
                         break;
-                    System.out.println(input);
                     data[i] = input;
                 }
 
-                matches.add(new matchUnits(data));
-                while(true){
-                    input= in.readLine();
-                    if(input==null)
+                matches[j] = new matchUnits(data);
+                while (true) {
+                    input = in.readLine();
+                    if (input == null)
                         break;
-                    if(input.contains("{"))
+                    if (input.contains("{"))
                         break;
                 }
-                if(input==null)
+                if (input == null)
                     break;
             }
             in.close();
