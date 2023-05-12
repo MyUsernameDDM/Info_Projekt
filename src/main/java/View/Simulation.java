840f8a721e68babc8dd9080842ad59281d153f8b
Stocker Daniel <dani002.stocker@gmail.com>
2023-05-12 21:28:56 +0200

    showChartInfoView, showInfoView

--- src/main/java/View/Simulation.java
+++ src/main/java/View/Simulation.java
@@ -1,14 +1,9 @@
 package View;
 
-import MainModel.Article;
-
-import java.util.ArrayList;
-
 /**
  * Klasse zum speichern der Daten einer Simulation, so sollen auch mehrere Simulationen gleichzeitg möglich sein.
  */
 public class Simulation {
-    ArrayList<Article> walletArticles = new ArrayList<>();
     static int moneyInv = 0;
     static int moneyAv = 1000;
     static int openShares = 0;
